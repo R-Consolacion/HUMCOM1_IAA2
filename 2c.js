270a1600718663372317091a7d58af0c38351143
Renato Consolacion <98303532+R-Consolacion@users.noreply.github.com>
2022-04-30 04:36:33 +0800

    Delete 2c.js

--- 2c.js
+++
@@ -1,8 +0,0 @@
-window.onload = writeMessage;
-function writeMessage() {
-    console.log("Renato Consolacion");
-    console.log("BS Information Technology");
-    console.log("1st Year");
-    console.log("HUMCOM1 - IAA2");
-    console.log("Human-Computer Interaction");
-}
