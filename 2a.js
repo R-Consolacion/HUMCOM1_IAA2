509eab04966e613f04d635e100b32a0f9717016e
Renato Consolacion <98303532+R-Consolacion@users.noreply.github.com>
2022-04-30 04:36:22 +0800

    Delete 2a.js

--- 2a.js
+++
@@ -1,8 +0,0 @@
-window.onload = writeMessage;
-function writeMessage() {
-    document.getElementById("target").innerHTML = "Renato Consolacion";
-        document.getElementById("demo").innerHTML = "BS Information Technology<br>" +
-                                                    "1st Year<br>" +
-                                                    "HUMCOM1 - IAA2<br>" +
-                                                    "Human-Computer Interaction<br>";
-}
