52335d960cf62f0d874730f06a5814afd68a9762
Renato Consolacion <98303532+R-Consolacion@users.noreply.github.com>
2022-04-30 04:34:12 +0800

    Exercise 14 with External JavaScript
    Exercise 14 with External JavaScript

---
+++ 2c.js
@@ -0,0 +1,8 @@
+window.onload = writeMessage;
+function writeMessage() {
+    console.log("Renato Consolacion");
+    console.log("BS Information Technology");
+    console.log("1st Year");
+    console.log("HUMCOM1 - IAA2");
+    console.log("Human-Computer Interaction");
+}
